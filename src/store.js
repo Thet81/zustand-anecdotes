564477f7dd1359de9d627e74b57a794cb8dddade
thetnaing <thetnaingoo1530@gmail.com>
2026-05-05 12:06:15 +0630

    implement unit test - 6.12 Anecdotes, step11

--- src/store.js
+++ src/store.js
@@ -64,3 +64,5 @@ export const useNotification = ()=> {
   }
   return notification
 }
+
+export default useAnecdoteStore;
---
+++ src/store.test.js
@@ -0,0 +1,50 @@
+// store.test.js
+
+import {beforeEach, describe, expect, it , vi} from 'vitest'
+// import anecdoteStore to use getState(), setState(), ...etc methods  on it
+import anecdoteStore,{useAnecdotes,useAnecdoteActions} from './store'
+import { renderHook, act } from '@testing-library/react'
+import anecdoteService from './services/anecdotes'
+
+vi.mock('./services/anecdotes',()=> ({
+	default : {
+		getAll : vi.fn(),
+		createNew : vi.fn(),
+		update : vi.fn()
+	}
+}))
+
+beforeEach(()=> {
+	anecdoteStore.setState({anecdotes : [], filter : "", notification : ""})
+	vi.clearAllMocks()
+})
+
+describe('use anecdotes action',()=> {
+	it ('initialize loads anecdotes from the server',async ()=> {
+		const mockAnecdotes = [{id : 1 , content : 'some anecdote for testing', votes : 0}]
+		anecdoteService.getAll.mockResolvedValue(mockAnecdotes)
+
+		const {result} = renderHook(()=> useAnecdoteActions())
+
+		await act(async ()=> {
+			await result.current.initialize()
+		})
+
+		const {result : anecdotesValue} = renderHook(()=> useAnecdotes())
+		expect(anecdotesValue.current).toEqual(mockAnecdotes)
+	})
+})
+// beforeEach(()=> {
+// 	anecdoteStore.setState({anecdotes : []});
+// })
+
+// describe("anecdoteStore",()=> {
+// 	it("initial state of anecdote is empty",()=> {
+// 		expect(anecdoteStore.getState().anecdotes.length).toBe(0)
+// 	})
+// 	it('useCounter returns initial value of 0', () => {
+//     const { result } = renderHook(() => useAnecdotes())
+//     console.log('render hook result is ', result)
+//     expect(result.current.length).toBe(0)
+//   })
+// })
