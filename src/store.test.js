2e5942c82ea61086616b680bc61ba2a00359f8c3
thetnaing <thetnaingoo1530@gmail.com>
2026-05-07 12:39:07 +0630

    done 6.14 Anecdotes, setp13

--- src/store.test.js
+++ src/store.test.js
@@ -2,7 +2,7 @@
 
 import {beforeEach, describe, expect, it , vi} from 'vitest'
 // import anecdoteStore to use getState(), setState(), ...etc methods  on it
-import anecdoteStore,{useAnecdotes,useAnecdoteActions} from './store'
+import useAnecdotesStore,{useAnecdotes,useAnecdoteActions} from './store'
 import { renderHook, act } from '@testing-library/react'
 import anecdoteService from './services/anecdotes'
 
@@ -15,7 +15,7 @@ vi.mock('./services/anecdotes',()=> ({
 }))
 
 beforeEach(()=> {
-	anecdoteStore.setState({anecdotes : [], filter : "", notification : ""})
+	useAnecdotesStore.setState({anecdotes : [], filter : "", notification : ""})
 	vi.clearAllMocks()
 })
 
@@ -54,16 +54,62 @@ describe('use anecdotes action',()=> {
 	})
 })
 // beforeEach(()=> {
-// 	anecdoteStore.setState({anecdotes : []});
+// 	useAnecdotesStore.setState({anecdotes : []});
 // })
 
 // describe("anecdoteStore",()=> {
 // 	it("initial state of anecdote is empty",()=> {
-// 		expect(anecdoteStore.getState().anecdotes.length).toBe(0)
+// 		expect(useAnecdotesStore.getState().anecdotes.length).toBe(0)
 // 	})
 // 	it('useCounter returns initial value of 0', () => {
 //     const { result } = renderHook(() => useAnecdotes())
 //     console.log('render hook result is ', result)
 //     expect(result.current.length).toBe(0)
 //   })
+// })
+const anecdotes = [
+		{id : 1 , content : 'anecdote to test', votes : 0},
+		{id : 2, content : 'yet another anecdote for testing', votes : 12}
+	]
+beforeEach(()=> {
+	useAnecdotesStore.setState({anecdotes})
+})
+
+describe("filtering",()=> {
+	it("returns all anecdotes with no filter",()=> {
+		const {result} = renderHook(()=> useAnecdotes())
+		expect(result.current).toHaveLength(2)
+		})
+
+	it ("filter the anecdote",()=> {
+		// it returns the anecdote that contains the word 'another'
+		useAnecdotesStore.setState({anecdotes, filter : 'another'})
+		const {result} = renderHook(()=> useAnecdotes())
+		expect(result.current).toHaveLength(1)
+		expect(result.current).toEqual([anecdotes[1]])
+	})
+
+	it("filter the anecdotes properly",()=> {
+		useAnecdotesStore.setState({anecdotes, filter : "anecdote"})
+		const {result} = renderHook(()=> useAnecdotes())
+		expect(result.current.length).toBe(2)
+		
+	})
+})
+
+
+
+// describe("filtering ", ()=> {
+// 	const anecdotes = [
+// 			{id : 1 , content : 'anecdote to test', votes : 0},
+// 			{id : 2, content : 'yet another anecdote for testing', votes : 12}
+// 		]
+// 	beforeEach(()=> {
+// 		useAnecdotesStore.setState({anecdotes})
+// 	})
+
+// 	it("returns all anecdotes with no filter", ()=> {
+// 		const {result} = renderHook(()=> useAnecdotes())
+// 		expect(result.current.anecdotes).toHaveLength(2)
+// 	})
 // })
